cd75c3bd4fe9288625b0474e110a1c533cbd33c4
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-06 17:38:31 +0300

    Update create_payment_method_table.sql

--- src/database/migrations/create_payment_method_table.sql
+++ src/database/migrations/create_payment_method_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE payment_method (
     method_name VARCHAR(50) NOT NULL CHECK (CHAR_LENGTH(method_name) BETWEEN 2 AND 50),
     description TEXT DEFAULT '',
     icon_url VARCHAR(255) NOT NULL DEFAULT '',
-    order INT DEFAULT 0,
+    "order" INT DEFAULT 0,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
