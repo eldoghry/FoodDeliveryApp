d116fc19da23a9c66fd2500f7c5c9844d72658bf
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-06 17:16:38 +0300

    Update create_payment_method_table.sql

--- src/database/migrations/create_payment_method_table.sql
+++ src/database/migrations/create_payment_method_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE payment_method (
     payment_method_id SERIAL PRIMARY KEY,
-    method_name VARCHAR(20) NOT NULL CHECK (CHAR_LENGTH(method_name) BETWEEN 2 AND 20),
+    method_name VARCHAR(50) NOT NULL CHECK (CHAR_LENGTH(method_name) BETWEEN 2 AND 50),
     description TEXT DEFAULT '',
     icon_url VARCHAR(255) NOT NULL DEFAULT '',
     order INT DEFAULT 0,
