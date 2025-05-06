3fb683862eb7faf606340cd5348575cf62f7a47e
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-06 17:29:23 +0300

    Update create_order_item_table.sql

--- src/database/migrations/create_order_item_table.sql
+++ src/database/migrations/create_order_item_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE order_item (
     order_item_id SERIAL PRIMARY KEY,
-    --order_id INT NOT NULL REFERENCES "order"(order_id),
-    -- item_id INT NOT NULL REFERENCES item(item_id),
+    order_id INT NOT NULL REFERENCES "order"(order_id),
+    item_id INT NOT NULL REFERENCES item(item_id),
     quantity INT NOT NULL CHECK (quantity > 0),
     item_price DECIMAL(10,2) NOT NULL CHECK (item_price >= 0.00),
     total_price DECIMAL(10,2) NOT NULL CHECK (total_price >= 0.00),
