8b90715f4a5f43beade9a2d015b48c653bc963c2
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-06 17:30:37 +0300

    remove comments in create_auditing_table.sql

--- src/database/migrations/create_auditing_table.sql
+++ src/database/migrations/create_auditing_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE auditing (
     auditing_id SERIAL PRIMARY KEY,
-    -- user_id INT NOT NULL REFERENCES "user"(user_id),
+    user_id INT NOT NULL REFERENCES "user"(user_id),
     audit_event JSONB NOT NULL DEFAULT '{}',
     audit_data JSONB NOT NULL DEFAULT '{}',
     audit_date TIMESTAMP NOT NULL,
