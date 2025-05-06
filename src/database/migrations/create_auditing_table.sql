f64823bb1c979cdffb564bc42d92f5b75abc06ff
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-06 17:07:24 +0300

    Update create_auditing_table.sql

--- src/database/migrations/create_auditing_table.sql
+++ src/database/migrations/create_auditing_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE auditing (
     audit_id SERIAL PRIMARY KEY,
     -- user_id INT NOT NULL REFERENCES "user"(user_id),
-    table_name VARCHAR(100) NOT NULL CHECK (CHAR_LENGTH(table_name) BETWEEN 2 AND 100),
-    row_id INT NOT NULL,
-    action VARCHAR(50) NOT NULL CHECK (CHAR_LENGTH(action) BETWEEN 2 AND 50),
-    changed_data JSONB NOT NULL DEFAULT '{}',
+    audit_event JSONB NOT NULL DEFAULT '{}',
+    audit_data JSONB NOT NULL DEFAULT '{}',
+    audit_date TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
