c37964a47de5ec2c2c9220cf3ef445b0bb9ce603
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-07 00:00:36 +0300

    Update create_cart_table.sql

--- src/database/migrations/create_cart_table.sql
+++ src/database/migrations/create_cart_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE cart (
     -- customer_id INT NOT NULL REFERENCES customer(customer_id),
     -- branch_id INT NOT NULL REFERENCES branch(branch_id),
     total_items INT NOT NULL CHECK (total_items > 0),
-    cart_discount DECIMAL(10,2) NOT NULL DEFAULT 0.0,
+    total_discount DECIMAL(10,2) NOT NULL DEFAULT 0.0,
     total_amount DECIMAL(10,2) NOT NULL CHECK (total_amount >= 0.00),
     restaurant_note TEXT DEFAULT '',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
