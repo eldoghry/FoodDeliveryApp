cf272f447d9f2e142c969777ddc845b29899d390
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-06 17:40:11 +0300

    Update create_transaction_detail_table.sql

--- src/database/migrations/create_transaction_detail_table.sql
+++ src/database/migrations/create_transaction_detail_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE transaction_detail (
     transaction_detail_id SERIAL PRIMARY KEY,
-    -- transaction_id INT NOT NULL REFERENCES transaction(transaction_id),
+    transaction_id INT NOT NULL REFERENCES transaction(transaction_id),
     detail_key JSONB NOT NULL,
     detail_value JSONB NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
