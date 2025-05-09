5e3198890401eb160c86bd873f962abd24da47c6
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-05-09 15:23:48 +0300

    fix: prettier and eslint

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,16 +2,16 @@ import { AppDataSource } from '../config/data-source';
 import { User } from '../models/user.entity';
 
 export class UserRepository {
-  private repo = AppDataSource.getRepository<User>('User');
+	private repo = AppDataSource.getRepository<User>('User');
 
-  async createUser(data: any) {
-    return {
-      name: 'Mohamed Magdy',
-      email: 'moh.mag.ali@gmail.com',
-    };
-  }
+	async createUser(data: any) {
+		return {
+			name: 'Mohamed Magdy',
+			email: 'moh.mag.ali@gmail.com'
+		};
+	}
 
-  async getAll() {
-    return this.repo.find();
-  }
+	async getAll() {
+		return this.repo.find();
+	}
 }
