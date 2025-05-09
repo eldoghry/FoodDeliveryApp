fb8f4242b425a51d5df05d0fa1c866ae3c5ebe37
Randa Magdy <48019673+randa-magdy@users.noreply.github.com>
2025-05-09 11:50:52 +0300

    fix: remove unnecessary columns from cart table

--- src/database/migrations/create_cart_table.sql
+++ src/database/migrations/create_cart_table.sql
@@ -2,10 +2,7 @@ CREATE TABLE cart (
     cart_id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL REFERENCES customer(customer_id),
     restaurant_id INT NOT NULL REFERENCES restaurant(restaurant_id),
-    total_items INT NOT NULL CHECK (total_items > 0),
-    total_discount DECIMAL(10,2) NOT NULL DEFAULT 0.0,
-    total_amount DECIMAL(10,2) NOT NULL CHECK (total_amount >= 0.00),
-    restaurant_note TEXT DEFAULT '',
+    total_items INT NOT NULL DEFAULT 0 CHECK (total_items >=0),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
