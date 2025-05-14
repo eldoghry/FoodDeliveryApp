ccd498f37eea0266385ef363830257199e9d03a5
Randa Magdy <randamagdy123@gmail.com>
2025-05-14 09:03:19 +0300

    fix: Refactored several tables to align with database best practices

--- src/database/migrations/2025-05-03_create_address_table.sql
+++ src/database/migrations/2025-05-03_create_address_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE address (
     address_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES "user"(user_id),
     address_line1 TEXT NOT NULL,
     address_line2 TEXT NOT NULL,
     city VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- src/database/migrations/create_cart_item_table.sql
+++ src/database/migrations/create_cart_item_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cart_item (
     cart_item_id SERIAL PRIMARY KEY,
     cart_id INT NOT NULL REFERENCES cart(cart_id),
-    item_id INT NOT NULL REFERENCES item(item_id),
+    menu_item_id INT NOT NULL REFERENCES menu_item(menu_item_id),
     quantity INT NOT NULL CHECK (quantity > 0),
     price DECIMAL(10,2) NOT NULL CHECK (price >= 0.00),
     discount DECIMAL(10,2) NOT NULL CHECK (price >= 0.00),
@@ -9,3 +9,4 @@ CREATE TABLE cart_item (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+ 
