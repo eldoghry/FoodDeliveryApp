3b8069f404ed301e96c73225db0ed6dec56e7e64
Randa Magdy <randamagdy123@gmail.com>
2025-05-16 19:53:47 +0300

    fix: remove comma exist after last field

--- src/database/migrations/2025-05-09_create_menu_item_table.sql
+++ src/database/migrations/2025-05-09_create_menu_item_table.sql
@@ -2,5 +2,5 @@
 CREATE TABLE menu_item (
     menu_item_id SERIAL PRIMARY KEY,
     menu_id INT NOT NULL REFERENCES menu(menu_id),
-    item_id INT NOT NULL REFERENCES item(item_id),
+    item_id INT NOT NULL REFERENCES item(item_id)
 );
