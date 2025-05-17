63aed983646633caf81adcfe1d4cce19d58b71f5
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2025-05-18 02:19:55 +0300

    feat(logging): Add logging to cart item removal

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -1,13 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import ApplicationError from '../errors/application.error';
-import { CartRepository } from '../repositories/cart.repository';
 import ErrMessages from '../errors/error-messages';
 import { AppDataSource } from '../config/data-source';
 import { Cart, CartItem } from '../models';
+import logger from '../config/logger';
 
 export class CartService {
 	async removeItem(cartItemId: number) {
+		logger.info(`Starting removal of cart item`, { cartItemId });
+
 		return await AppDataSource.transaction(async (transactionalEntityManager) => {
+			logger.info(`Beginning transaction for cart item removal`, { cartItemId });
+
 			const cartItem = await transactionalEntityManager.findOne(CartItem, {
 				where: { cartItemId },
 				relations: ['cart']
@@ -28,6 +32,7 @@ export class CartService {
 				throw new ApplicationError(ErrMessages.cart.CartNotActive, StatusCodes.BAD_REQUEST);
 			}
 
+			logger.info(`Deleting cart item`, { cartItemId });
 			await transactionalEntityManager.delete(CartItem, cartItemId);
 
 			const cartItems = await transactionalEntityManager.find(CartItem, {
@@ -37,6 +42,12 @@ export class CartService {
 			const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 
 			await transactionalEntityManager.update(Cart, cart.cartId, { totalItems });
+
+			logger.info(`Successfully removed cart item and updated cart totals`, {
+				cartItemId,
+				cartId: cart.cartId,
+				newTotalItems: totalItems
+			});
 		});
 	}
 }
