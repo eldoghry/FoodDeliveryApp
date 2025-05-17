77435221c780a0683c431e311f8f29c3bd5d44fb
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2025-05-18 02:02:32 +0300

    feat(cart): Add transaction management for cart item removal

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -2,29 +2,41 @@ import { StatusCodes } from 'http-status-codes';
 import ApplicationError from '../errors/application.error';
 import { CartRepository } from '../repositories/cart.repository';
 import ErrMessages from '../errors/error-messages';
+import { AppDataSource } from '../config/data-source';
+import { Cart, CartItem } from '../models';
 
 export class CartService {
-	private cartRepo = new CartRepository();
-
 	async removeItem(cartItemId: number) {
-		const cartItem = await this.cartRepo.getCartItem(cartItemId);
+		return await AppDataSource.transaction(async (transactionalEntityManager) => {
+			const cartItem = await transactionalEntityManager.findOne(CartItem, {
+				where: { cartItemId },
+				relations: ['cart']
+			});
 
-		if (!cartItem) {
-			throw new ApplicationError(ErrMessages.cart.CartItemNotFound, StatusCodes.NOT_FOUND);
-		}
+			if (!cartItem) {
+				throw new ApplicationError(ErrMessages.cart.CartItemNotFound, StatusCodes.NOT_FOUND);
+			}
 
-		const cart = await this.cartRepo.getCartById(cartItem.cartId);
-		if (!cart) {
-			throw new ApplicationError(ErrMessages.cart.CartNotFound, StatusCodes.NOT_FOUND);
-		}
+			const cart = cartItem.cart;
+			if (!cart) {
+				throw new ApplicationError(ErrMessages.cart.CartNotFound, StatusCodes.NOT_FOUND);
+			}
 
-    // TODO: validate that the customer owns the cart
+			// TODO: validate that the customer owns the cart
 
-		if (!cart.isActive) {
-			throw new ApplicationError(ErrMessages.cart.CartNotActive, StatusCodes.BAD_REQUEST);
-		}
+			if (!cart.isActive) {
+				throw new ApplicationError(ErrMessages.cart.CartNotActive, StatusCodes.BAD_REQUEST);
+			}
 
-		await this.cartRepo.deleteCartItem(cartItemId);
-		await this.cartRepo.updateCartTotalItems(cart.cartId);
+			await transactionalEntityManager.delete(CartItem, cartItemId);
+
+			const cartItems = await transactionalEntityManager.find(CartItem, {
+				where: { cartId: cart.cartId }
+			});
+
+			const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+			await transactionalEntityManager.update(Cart, cart.cartId, { totalItems });
+		});
 	}
 }
