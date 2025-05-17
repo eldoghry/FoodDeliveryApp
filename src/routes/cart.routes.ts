c63847ab70708179dfe89606330635b4389c199b
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-05-17 15:41:24 +0300

    feat: add swagger to add to cart route

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -8,6 +8,35 @@ import { isRestaurantAvailable } from '../middlewares/isRestaurantAvailable.midd
 const CartRouter = Router();
 const controller = new CartController();
 
+/**
+ * @swagger
+ * /cart/add:
+ *   post:
+ *     summary: Add an item to the user's cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/AddItemToCart'
+ *     responses:
+ *       200:
+ *         description: Item added to cart successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/messageResponse'
+ *       400:
+ *         description: Invalid request body
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Restaurant not available
+ */
 CartRouter.post(
 	'/add',
 	isAuthenticated,
