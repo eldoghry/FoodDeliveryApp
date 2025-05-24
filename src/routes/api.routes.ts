ac30be38f5eb7a4fdd8d73b0c7220e14fe6feb15
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-05-24 19:07:33 +0300

    chore: prepare order controller, routes and service

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -3,6 +3,7 @@ import AppRouter from './app.routes';
 import UserRouter from './user.routes';
 import CartRouter from './cart.routes';
 import AuthRouter from './auth.routes';
+import OrderRouter from './order.routes';
 
 const ApiRouter = Router();
 
@@ -10,6 +11,6 @@ ApiRouter.use('/app', AppRouter);
 ApiRouter.use('/auth', AuthRouter);
 ApiRouter.use('/user', UserRouter);
 ApiRouter.use('/cart', CartRouter);
-ApiRouter.use('/cart', CartRouter);
+ApiRouter.use('/order', OrderRouter);
 
 export default ApiRouter;
