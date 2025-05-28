022075ba242c9a076e1b4bb97afa583d13fe9800
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-05-29 00:59:53 +0300

    refactor: cleanup

--- src/payment/paypal/paypal.axios.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '../../config/env';
-import { BaseAxios } from '../../shared/baseAxios';
-
-export class PaypalAxios extends BaseAxios {
-	constructor() {
-		const baseUrl = config.payment.paypal.baseUrl;
-		super('PAYPAL', baseUrl);
-	}
-}
--- src/payment/paypal/paypal.request.ts
+++
@@ -1,5 +0,0 @@
-export const PaypalCreateOrderUrl = '/v2/checkout/orders';
-export const PaypalOrderDetailsUrl = (orderId: string) => `/v2/checkout/orders/${orderId}`;
-export const PaypalConfirmOrderUrl = (orderId: string) => `/v2/checkout/orders/${orderId}/confirm-payment-source`;
-export const PaypalCaptureOrder = (orderId: string) => `/v2/checkout/orders/${orderId}/capture`;
-export const PaypalAuthUrl = '/v1/oauth2/token'
