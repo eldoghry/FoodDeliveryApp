681877b074b69651ea4129733d76d46644b80fce
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-06-13 15:34:15 +0300

    chore: integrate redis with setting

--- src/services/setting.service.ts
+++ src/services/setting.service.ts
@@ -4,17 +4,29 @@ import ApplicationError from '../errors/application.error';
 import ErrMessages from '../errors/error-messages';
 import { SettingRepository } from '../repositories';
 import { Setting } from '../models';
+import { redisService } from '../shared/redis';
 
 export class SettingService {
 	private static settingRepo = new SettingRepository();
+	static REDIS_PREFIX = 'settings';
 
 	static async get(key: string): Promise<any> {
 		const sanitizedKey = this.sanitizeKey(key);
+		// get from redis first
+
+		const cachedKey = redisService.generateKey(this.REDIS_PREFIX, sanitizedKey);
+		const cachedValue = await redisService.get(cachedKey, true);
+		if (cachedValue !== null) return cachedValue;
+
+		// get from database
 		const setting = await this.settingRepo.findByKey(sanitizedKey);
 
 		if (!setting) throw new ApplicationError(ErrMessages.setting.SettingNotFound, HttpStatusCodes.NOT_FOUND);
 
-		return setting.value;
+		// set to redis
+		const result = setting.value;
+		await redisService.set(cachedKey, result);
+		return result;
 	}
 
 	static async getAll(): Promise<Setting[]> {
@@ -23,12 +35,17 @@ export class SettingService {
 
 	static async set(key: string, value: any, description?: string): Promise<Setting> {
 		const sanitizedKey = this.sanitizeKey(key);
-		return this.settingRepo.upsertByKey(sanitizedKey, value, description);
+		const updated = await this.settingRepo.upsertByKey(sanitizedKey, value, description);
+		const cachedKey = redisService.generateKey(this.REDIS_PREFIX, sanitizedKey);
+		await redisService.set(cachedKey, updated.value);
+		return updated;
 	}
 
 	static async delete(key: string): Promise<void> {
 		const sanitizedKey = this.sanitizeKey(key);
 		await this.settingRepo.delete(sanitizedKey);
+		const cachedKey = redisService.generateKey(this.REDIS_PREFIX, sanitizedKey);
+		await redisService.del(cachedKey);
 	}
 
 	private static sanitizeKey(key: string): string {
