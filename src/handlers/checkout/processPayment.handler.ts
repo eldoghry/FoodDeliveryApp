91d8744c731b235a2f402dc60979da93506cccfc
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-06-14 17:05:49 +0300

    chore: initiate transaction on payment processor handler

--- src/handlers/checkout/processPayment.handler.ts
+++ src/handlers/checkout/processPayment.handler.ts
@@ -1,17 +1,34 @@
-import { PaymentService } from '../../services/payment/payment.service';
+import { Transaction } from 'typeorm';
+import { TransactionPaymentStatus } from '../../enums/transaction.enum';
+import { PaymentHandler } from '../../services/payment/payment.handler';
+import { TransactionService } from '../../services/transaction.service';
 import { CheckoutHandler, CheckoutContext } from './checkout.handler';
 
 export class ProcessPaymentHandler extends CheckoutHandler {
+	private transactionService = new TransactionService();
+
 	constructor() {
 		super();
 	}
 
 	async handleRequest(context: CheckoutContext) {
-		const paymentService = new PaymentService(context.payload.paymentMethod);
-		context.processPaymentResult = await paymentService.processPayment(context.totalAmount!, {
+		const paymentHandler = new PaymentHandler(context.payload.paymentMethod);
+		context.processPaymentResult = await paymentHandler.processPayment(context.totalAmount!, {
 			items: context.cart!.cartItems,
 			order: context.order
 		});
+
+		// Create transaction record
+		const transaction = await this.transactionService.createTransaction({
+			amount: context.totalAmount!,
+			status: TransactionPaymentStatus.INITIATED,
+			orderId: context.order?.orderId,
+			customerId: context?.customer?.customerId,
+			paymentMethodCode: context.payload.paymentMethod,
+			paymentReference: context.processPaymentResult.paymentId
+		});
+
+		context.transaction = transaction;
 
 		return context;
 	}
