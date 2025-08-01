de183ac8ba6f7c82a8f69b5a4ca386bea560b38c
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-08-01 18:27:29 +0300

    chore: adjust ecosystem config to run on cluster mode with 3 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,10 @@ module.exports = {
 		{
 			name: 'food-delivery',
 			script: 'dist/src/server.js',
-			instances: 1,
-			exec_mode: 'fork', // use "cluster" for multi-core
+			instances: 3,
+			// exec_mode: 'fork', // use "cluster" for multi-core
+			exec_mode: 'cluster', // use "cluster" for multi-core
+			instance_var: 'INSTANCE_ID',
 			watch: false,
 			env: {
 				NODE_ENV: 'production'
