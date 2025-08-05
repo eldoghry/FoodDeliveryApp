968c3c77628c56d04f19792527c44ee5a0d43956
Randa Magdy <randamagdy123@gmail.com>
2025-08-05 11:19:56 +0300

    security(express): limit JSON body size

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ export const createApp = (): Application => {
 	// register middlewares
 	app.use(addRequestTimeMiddleware); // add request time
 	app.set('trust proxy', '127.0.0.1');
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json({ limit: '1mb' })); // with limit json size for security
+	app.use(express.urlencoded({ extended: true, limit: '1mb' })); 
 	app.use(cors());
 	app.use(helmet());
 	app.use(morgan('dev'));
